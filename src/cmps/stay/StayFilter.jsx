94e24001ba6a0332ba6ed8f5114a2b59d572307d
shermanido37 <shermanido37@gmail.com>
2025-03-26 17:53:40 +0200

    working non-styled buttons

--- src/cmps/stay/StayFilter.jsx
+++ src/cmps/stay/StayFilter.jsx
@@ -1,7 +1,47 @@
-
+import { useEffect, useRef } from "react"
+import { useSearchParams } from "react-router-dom"
 
 export function StayFilter() {
-    return(
-        <section>StayFilter</section>
+    const [searchParams, setSearchParams] = useSearchParams()
+    const IMG_URL_FORMAT = "../../src/assets/img/stay/type/"
+    const typeList = [
+        "OMG!",
+        "Beachfront",
+        "Amazing Views"
+    ]
+
+    useEffect(() => {
+        console.log(typeList)
+        const typeFromParams = searchParams.get("type")
+        console.log("type from params: " + typeFromParams)
+        if(typeFromParams !== null && typeFromParams != ""){
+            selectType(typeFromParams)
+        }
+    }, [])
+
+    function onClickType(type) {
+        setSearchParams({ type })
+        selectType(type)
+    }
+
+    function selectType(type){
+        console.log("selecting type " + type)
+    }
+
+    return (
+        <section>
+            <h3>StayFilter</h3>
+            <ul>
+                {
+                    typeList.map(type => (
+                        <li key={type} onClick={() => onClickType(type)} className="">
+                            <img src={`${IMG_URL_FORMAT}${type}.jpg`} alt="" />
+                            <span>{type}</span>
+                        </li>
+                    ))
+                }
+            </ul>
+
+        </section>
     )
 }
