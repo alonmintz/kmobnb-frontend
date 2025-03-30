8cfbd1e438f2b399e67b7c3bc0729de4dd2d7577
keyal <e2lk@proton.me>
2025-03-30 22:50:09 +0300

    groundwork for using stays in StayList WIP

--- src/cmps/stay/StayFilter.jsx
+++ src/cmps/stay/StayFilter.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useEffectUpdate } from "../../customHooks/useEffectUpdate";
-import {Carousel, WithStyles} from "react-multi-carousel";
+import { Carousel, WithStyles } from "react-multi-carousel";
 
 export function StayFilter({ filterBy, onSetFilterBy }) {
   const IMG_URL_FORMAT = "../../src/assets/img/stay/type/";
--- src/cmps/stay/StayList.jsx
+++ src/cmps/stay/StayList.jsx
@@ -1,8 +1,25 @@
-import { StayPreview } from "./StayPreview";
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { loadStays } from "../../store/actions/stay.actions"
+import { StayPreview } from "./StayPreview"
 
 export function StayList() {
     //TODO: logic to pull and render multiple StayPreviews
-    return(
+    const stays = useSelector((storeState) => storeState.stayModule.stays)
+    const filterBy = useSelector((storeState) => storeState.stayModule.filterBy)
+    // const dispatch = useDispatch()
+
+    useEffect(() => {
+        loadStays(filterBy)
+            .then(() => {
+                console.log('Stays loaded successfully')
+            })
+            .catch((err) => {
+                console.error('Error loading stays:', err)
+            })
+    },[filterBy])
+
+    return (
         <div className="stay-list">
             <StayPreview />
             <StayPreview />
