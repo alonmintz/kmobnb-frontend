23c45e5121e5cb65c267528f68ca7f545b6cd0be
shermanido37 <shermanido37@gmail.com>
2025-03-30 21:10:07 +0300

    merge from main

--- src/cmps/stay/StayFilter.jsx
+++ src/cmps/stay/StayFilter.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useEffectUpdate } from "../../customHooks/useEffectUpdate";
+import {Carousel, WithStyles} from "react-multi-carousel";
 
 export function StayFilter({ filterBy, onSetFilterBy }) {
   const IMG_URL_FORMAT = "../../src/assets/img/stay/type/";
@@ -31,7 +32,7 @@ export function StayFilter({ filterBy, onSetFilterBy }) {
 
   //  TODO: notify parent when filterBy is updated
   useEffectUpdate(() => {
-    // onSetFilterBy(filterByToEdit);
+    onSetFilterBy(filterByToEdit);
   }, [filterByToEdit]);
 
   function onClickType(type) {
@@ -48,13 +49,6 @@ export function StayFilter({ filterBy, onSetFilterBy }) {
   return (
     <section className="stay-filter">
       <h3>StayFilter</h3>
-      {/* this code is the start of a form for setting filterBy */}
-      {/* <form onSubmit={handleSubmit}>
-        <fieldset id="type_select">
-            <input type="radio"></input>
-        </fieldset>
-      </form> */}
-      {/* this code creates a list of simple buttons for each type */}
       <div className="stay-filter-carousel">
         <button className="back" aria-label="Scroll to previous stay types">back</button>
         <ul className="stay-filter-list">
