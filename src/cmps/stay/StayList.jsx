0bd839c466743fe3e69c715a406b66904e751dfc
keyal <e2lk@proton.me>
2025-04-01 02:48:31 +0300

    Utilize stays to generate list and preview, removed all logic from StayList, minor scss

--- src/cmps/stay/StayList.jsx
+++ src/cmps/stay/StayList.jsx
@@ -1,30 +1,13 @@
-import { useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { loadStays } from "../../store/actions/stay.actions"
 import { StayPreview } from "./StayPreview"
 
-export function StayList() {
-    //TODO: logic to pull and render multiple StayPreviews
-    const stays = useSelector((storeState) => storeState.stayModule.stays)
-    const filterBy = useSelector((storeState) => storeState.stayModule.filterBy)
-    // const dispatch = useDispatch()
+export function StayList({ stays }) {
 
-    useEffect(() => {
-        loadStays(filterBy)
-            .then(() => {
-                console.log('Stays loaded successfully')
-            })
-            .catch((err) => {
-                console.error('Error loading stays:', err)
-            })
-    },[filterBy])
-
+    if (!stays || !stays.length) return <div className="stay-list">No stays to show</div>
     return (
         <div className="stay-list">
-            <StayPreview />
-            <StayPreview />
-            <StayPreview />
-            <StayPreview />
+            {stays.map((stay) => (
+                <StayPreview key={stay._id} stay={stay} />
+            ))}
         </div>
     )
 }
