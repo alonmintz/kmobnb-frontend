4e0e69a6d47677287a6b31bb3bae90a3e6654840
alonmintz <92505398+alonmintz@users.noreply.github.com>
2025-04-05 02:11:16 +0300

    completed date picker + fixed z-index of stay image gallery buttons

--- src/cmps/stay/SearchDatePicker.jsx
+++ src/cmps/stay/SearchDatePicker.jsx
@@ -1,24 +1,28 @@
 import { SearchPickerWrapper } from "./SearchPickerWrapper";
-import { DateRange } from "react-date-range";
-import "react-date-range/dist/styles.css";
-import "react-date-range/dist/theme/default.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export function SearchDatePicker({ type, onSelect, ranges }) {
-  //TODO: complete datePicker styling
+export function SearchDatePicker({ type, dates, onSelect }) {
+  function onDateChange() {}
   return (
     <SearchPickerWrapper type={type}>
       <section className="search-date-picker">
-        <DateRange
-          editableDateInputs={true}
-          onChange={(item) => onSelect({ type, item })}
-          showSelectionPreview={true}
-          showPreview={true}
-          moveRangeOnFirstSelection={false}
-          months={2}
-          ranges={ranges}
-          direction="horizontal"
-          showDateDisplay={false}
+        <DatePicker
+          selected={dates[0]}
+          onChange={(dates) => onSelect({ type, dates })}
           minDate={new Date()}
+          startDate={dates[0]}
+          endDate={dates[1]}
+          monthsShown={2}
+          selectsRange
+          inline
+          showDisabledMonthNavigation
+          renderDayContents={(day, date) => (
+            <div className="day-wrapper">
+              <span className="number">{day}</span>
+              <div className="day-hover-circle"></div>
+            </div>
+          )}
         />
       </section>
     </SearchPickerWrapper>
