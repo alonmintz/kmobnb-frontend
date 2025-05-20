4c1011b71ab99e839ea91e1c211fd4ccd9b103c6
keyal <e2lk@proton.me>
2025-05-20 04:10:47 +0300

    apply login modal for button in OrderPage

--- src/pages/stay/OrderPage.jsx
+++ src/pages/stay/OrderPage.jsx
@@ -12,6 +12,7 @@ import { subDays, format } from "date-fns";
 import starIcon from "../../assets/img/rating-star.svg";
 import visaIcon from "../../assets/img/order/visa.svg";
 import { orderService } from "../../services/order/order,service.local";
+import { LoginSignupModal } from "../loginSignup/LoginSignupModal";
 
 //TODO: move to order service:
 const DAILY_FEE = 4;
@@ -19,6 +20,7 @@ const DAILY_FEE = 4;
 export function OrderPage() {
   const [isLoading, setIsLoading] = useState(true);
   const loggedInUser = useSelector((storeState) => storeState.userModule.user);
+  const [isLoginModalVisible, setIsLoginModalVisible] = useState(false)
   const [searchParams, setSearchParams] = useSearchParams();
   const params = useParams();
   const [stayToOrder, setStayToOrder] = useState();
@@ -235,9 +237,8 @@ export function OrderPage() {
       <div className="price-calc">
         <h4 className="title">price details</h4>
         <h4 className="desc">
-          <span>{`${pricePerNight}x${nightsCount} night${
-            nightsCount > 1 && "s"
-          }`}</span>
+          <span>{`${pricePerNight}x${nightsCount} night${nightsCount > 1 && "s"
+            }`}</span>
           <span>{`$${pricePerNights}`}</span>
         </h4>
         <h4 className="desc">
@@ -253,10 +254,6 @@ export function OrderPage() {
       </div>
     </section>
   );
-
-  function onLoginClick() {
-    //TODO: Eyal complete :)
-  }
 
   function onSubmit() {
     if (isOrderComplete) {
@@ -282,7 +279,7 @@ export function OrderPage() {
   function renderOrderButton() {
     if (!loggedInUser) {
       return (
-        <button className="submit-btn" onClick={onLoginClick}>
+        <button className="submit-btn" onClick={() => setIsLoginModalVisible(true)}>
           Please login
         </button>
       );
@@ -300,6 +297,7 @@ export function OrderPage() {
 
   return (
     <section className="order-page">
+      {!isLoginModalVisible ? "" : <LoginSignupModal onClose={() => setIsLoginModalVisible(false)} />}
       <div className="back-container">
         <Link
           className="back-link"
