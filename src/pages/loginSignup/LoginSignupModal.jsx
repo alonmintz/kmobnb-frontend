29e05f6706d984de2fa8030dcb5d6eddc1e5195f
keyal <e2lk@proton.me>
2025-05-20 03:55:00 +0300

    improve LoginSignupModal styling

--- src/pages/loginSignup/LoginSignupModal.jsx
+++ src/pages/loginSignup/LoginSignupModal.jsx
@@ -5,8 +5,18 @@ export function LoginSignupModal({ onClose }) {
 
   return (
     <Modal isBackdrop onClose={() => onClose()}>
-      <div className="login-modal">
-        <button className="modal-button" onClick={() => userActions.login({ username: "muki" })}>Login as demo guest</button>
+      <div className="login-modal-container">
+        <header className="modal-header">
+          <h1>Log in or sign up</h1>
+        </header>
+        <div className="login-modal-content">
+          <h2>Welcome to Kmobnb</h2>
+          <button className="modal-button" onClick={() => userActions.login({ username: "muki" })}>Login as demo guest</button>
+          <div className="divider">or</div>
+          <div className="auth-buttons">
+            <button className="email">Continue with email</button>
+          </div>
+        </div>
       </div>
     </Modal>
   );
