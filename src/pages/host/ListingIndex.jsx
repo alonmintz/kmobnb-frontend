8b54ab4cbc0bf1a75c6ba30d44a72b596819b795
keyal <e2lk@proton.me>
2025-05-26 01:54:22 +0300

    ListingIndex: reorg buttons, some on hover with effects. determine whether active

--- src/pages/host/ListingIndex.jsx
+++ src/pages/host/ListingIndex.jsx
@@ -3,6 +3,8 @@ import { stayActions } from "../../store/actions/stay.actions";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBan, faPen, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 
 export function ListingIndex() {
   const navigate = useNavigate()
@@ -16,11 +18,6 @@ export function ListingIndex() {
         .catch(err => console.log('Failed to load listings:', err))
     }
   }, [user])
-
-  function onViewListingAsGuestClick(ev, listingId) {
-    ev.stopPropagation()
-    navigate(`/stay/${listingId}`)
-  }
 
   if (!listings || !listings.length || !user) {
     return (
@@ -36,23 +33,51 @@ export function ListingIndex() {
     <section className="listings">
       <div className="section-title">
         <h1>Your Listings</h1>
+        <Link className="button">Add listing</Link>
       </div>
       <div className="listing-list">
         {listings.map((listing) =>
-          <div
-            key={listing._id}
-            className="listing-preview"
-            onClick={() => navigate(`/host/orders?listingId=${listing._id}`)}
-          >
-            <img src={listing.imgUrls[0]} />
+          <div key={listing._id} className={`listing-preview`}>
+            <div className="img-container">
+              <img src={listing.imgUrls[0]} />
+              <div className="hover-buttons">
+                <button title="Edit" className="round-btn edit-btn" onClick={(ev) => {
+                  ev.stopPropagation()
+                  navigate(`/host/listing/edit/${listing._id}`)
+                }}>
+                  <FontAwesomeIcon icon={faPen} />
+                </button>
+                {listing.status === 'active' ? (
+                  <button
+                    title="Deactivate"
+                    className="round-btn deactivate-btn"
+                    onClick={(ev) => {
+                      ev.stopPropagation()
+                      // TODO: add deactivate functionality here
+                    }}
+                  >
+                    <FontAwesomeIcon className="icon" icon={faBan} />
+                  </button>
+                ) : (
+                  <button
+                    title="Reactivate"
+                    className="round-btn reactivate-btn"
+                    onClick={(ev) => {
+                      ev.stopPropagation()
+                      // TODO: add reactivate functionality here
+                    }}
+                  >
+                    <FontAwesomeIcon className="icon" icon={faRotateLeft} />
+                  </button>
+                )}
+              </div>
+            </div>
             <div className="listing-name">{listing.name}</div>
-            <div className="listing-location"> {listing.loc.country}, {listing.loc.city}</div>
+            <div className="listing-location">{listing.loc.country}, {listing.loc.city}</div>
             <Link
-              to={`/host/listing/edit/${listing._id}`}
-              onClick={(ev => ev.stopPropagation())}
+              to={`/host/orders?listingId=${listing._id}`}
               className="button"
-            >
-              Edit Listing
+            >View orders
             </Link>
             <Link
               to={`/stay/${listing._id}`}
