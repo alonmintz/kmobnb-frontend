1300a1b66502f788ee4b89b374e719768eb55efa
keyal <e2lk@proton.me>
2025-06-02 21:19:42 +0300

    move all user-related sessionStorage to localStorage

--- src/services/user/user.service.remote.js
+++ src/services/user/user.service.remote.js
@@ -54,12 +54,12 @@ async function signup(userInput) {
 }
 
 async function logout() {
-  sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER);
+  localStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER);
   return await httpService.post("auth/logout");
 }
 
 function getLoggedinUser() {
-  return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER));
+  return JSON.parse(localStorage.getItem(STORAGE_KEY_LOGGEDIN_USER));
 }
 
 function saveLoggedinUser(user) {
@@ -70,7 +70,7 @@ function saveLoggedinUser(user) {
   //   isHost: user.isHost,
   //   isAdmin: user.isAdmin,
   // };
-  sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user));
+  localStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user));
   return user;
 }
 
